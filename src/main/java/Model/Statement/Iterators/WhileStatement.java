5716cd00af3c9f060bdea88366d213f7235b26fc
Daryl110 <daryl.8220@gmail.com>
2019-10-03 18:12:51 +0100

    [resolves #21] Sentecia de iteracion while

---
+++ src/main/java/Model/Statement/Iterators/WhileStatement.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.Statement.Iterators;
+
+import Model.Lexeme;
+import Model.LexemeTypes;
+import Model.Statement.BlockStatement;
+import Model.Statement.Expression.ExpressionStatement;
+import Model.Statement.Others.ReturnStatement;
+import Model.Statement.Structure.Statement;
+import Model.Statement.Structure.SyntacticTypes;
+import Model.TokensFlow;
+
+/**
+ *
+ * @author Daryl Ospina
+ */
+public class WhileStatement extends Statement {
+
+    public WhileStatement(Statement root) {
+        super(root);
+    }
+
+    public WhileStatement(Statement root, int positionBack) {
+        super(root, positionBack);
+    }
+
+    @Override
+    public String toString() {
+        return SyntacticTypes.WHILE_STATEMENT;
+    }
+
+    @Override
+    public Statement analyze(TokensFlow tokensFlow, Lexeme lexeme) {
+        if (lexeme != null && (lexeme.getType().equals(LexemeTypes.ITERATIVE_CONTROL_STRUCTURE)
+                && lexeme.getWord().equals("while"))) {
+            this.childs.add(lexeme);
+            lexeme = tokensFlow.move();
+
+            if (lexeme != null && lexeme.getType().equals(LexemeTypes.OPEN_PARENTHESIS)) {
+                this.childs.add(lexeme);
+
+                Statement logicalOrRelational = new ExpressionStatement(this, tokensFlow.getPositionCurrent());
+                logicalOrRelational = logicalOrRelational.analyze(tokensFlow, tokensFlow.move());
+                if (logicalOrRelational != null && (logicalOrRelational.toString().equals(SyntacticTypes.LOGICAL_EXPRESSION_STATEMENT)
+                        || logicalOrRelational.toString().equals(SyntacticTypes.RELATIONAL_EXPRESSION_STATEMENT))) {
+                    this.childs.add(logicalOrRelational);
+                    lexeme = tokensFlow.getCurrentToken();
+
+                    if (lexeme != null && lexeme.getType().equals(LexemeTypes.CLOSE_PARENTHESIS)) {
+                        this.childs.add(lexeme);
+                        lexeme = tokensFlow.move();
+
+                        if (lexeme != null && lexeme.getType().equals(LexemeTypes.OPEN_BRACES)) {
+                            this.childs.add(lexeme);
+                            lexeme = tokensFlow.move();
+
+                            while (lexeme != null && !lexeme.getType().equals(LexemeTypes.CLOSE_BRACES)) {
+
+                                if ((lexeme.getType().equals(LexemeTypes.FUNCTIONS) && lexeme.getWord().equals("return"))
+                                        || (lexeme.getType().equals(LexemeTypes.OTHERS) && (lexeme.getWord().equals("break")
+                                        || lexeme.getWord().equals("continue")))) {
+
+                                    if (lexeme.getWord().equals("return")) {
+                                        Statement returnStatement = new ReturnStatement(this, tokensFlow.getPositionCurrent());
+                                        returnStatement = returnStatement.analyze(tokensFlow, lexeme);
+                                        if (returnStatement != null) {
+                                            this.childs.add(returnStatement);
+                                            lexeme = tokensFlow.getCurrentToken();
+                                            break;
+                                        } else {
+                                            if (this.positionBack != -1) {
+                                                tokensFlow.moveTo(this.positionBack);
+                                            } else {
+                                                tokensFlow.backTrack();
+                                            }
+                                            return null;
+                                        }
+                                    }
+
+                                    this.childs.add(lexeme);
+                                    lexeme = tokensFlow.move();
+
+                                    if (lexeme != null && lexeme.getType().equals(LexemeTypes.DELIMITERS)) {
+                                        this.childs.add(lexeme);
+                                        lexeme = tokensFlow.move();
+                                        break;
+                                    } else {
+                                        if (this.positionBack != -1) {
+                                            tokensFlow.moveTo(this.positionBack);
+                                        } else {
+                                            tokensFlow.backTrack();
+                                        }
+                                        return null;
+                                    }
+                                }
+
+                                Statement statement = new BlockStatement(this, tokensFlow.getPositionCurrent());
+                                statement = statement.analyze(tokensFlow, lexeme);
+                                if (statement != null) {
+                                    this.childs.add(statement);
+                                    lexeme = tokensFlow.getCurrentToken();
+                                }
+                            }
+
+                            if (lexeme != null && lexeme.getType().equals(LexemeTypes.CLOSE_BRACES)) {
+                                this.childs.add(lexeme);
+                                tokensFlow.move();
+
+                                return this;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (this.positionBack != -1) {
+            tokensFlow.moveTo(this.positionBack);
+        } else {
+            tokensFlow.backTrack();
+        }
+        return null;
+    }
+
+}
---
+++ src/test/java/Model/Statement/Iterators/WhileStatementTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model.Statement.Iterators;
+
+import Model.Lexeme;
+import Model.LexemeTypes;
+import Model.Statement.Structure.Statement;
+import Model.TokensFlow;
+import java.util.ArrayList;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Daryl Ospina
+ */
+public class WhileStatementTest {
+
+    @Test
+    public void testAnalyze() {
+        System.out.println("analyze");
+        
+        ArrayList<Lexeme> lexemes = new ArrayList<>();
+        
+        lexemes.add(new Lexeme(0, 0, "while", LexemeTypes.ITERATIVE_CONTROL_STRUCTURE));
+        lexemes.add(new Lexeme(0, 0, "(", LexemeTypes.OPEN_PARENTHESIS));
+        lexemes.add(new Lexeme(0, 0, "i", LexemeTypes.IDENTIFIERS));
+        lexemes.add(new Lexeme(0, 0, "<", LexemeTypes.RELATIONAL_OPERATORS));
+        lexemes.add(new Lexeme(0, 0, "5", LexemeTypes.NUMBERS));
+        lexemes.add(new Lexeme(0, 0, ")", LexemeTypes.CLOSE_PARENTHESIS));
+        lexemes.add(new Lexeme(0, 0, "{", LexemeTypes.OPEN_BRACES));
+        lexemes.add(new Lexeme(0, 0, "break", LexemeTypes.OTHERS));
+        lexemes.add(new Lexeme(0, 0, ";", LexemeTypes.DELIMITERS));
+        lexemes.add(new Lexeme(0, 0, "}", LexemeTypes.CLOSE_BRACES));
+        
+        TokensFlow tokensFlow = new TokensFlow(lexemes);
+        WhileStatement forStatement = new WhileStatement(null);
+        
+        assertEquals(8, forStatement.analyze(tokensFlow, tokensFlow.getCurrentToken()).getChildCount());
+    }
+    
+}
